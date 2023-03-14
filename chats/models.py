48844797b443ca982219996f1f89edb41755a954
saurabhraturi2000 <saurabh.raturi.999@gmail.com>
2023-03-14 13:12:14 +0530

    pages connected

--- chats/models.py
+++ chats/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=255,blank=False)
+    email = models.EmailField(unique=True,blank=False)
+    password = models.CharField(max_length=255,blank=False)
+
+    def __str__(self):
+        return self.name
+
+# class Rooms(models.Model):
+#     room_name = models.CharField(max_length=255 , unique=True)
+
+#     def __str__(self):
+#         render = self.room_name
+
+# class messages(models.Model):
+#     room
+#     messages = models.TextField(max_length=300)
+#     datetime = models.DateTimeField(datetime=datetime.now())
--- chats/views.py
+++ chats/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'chats/index.html')
+
+def login(request):
+    return render(request, 'chats/login.html')
+
+def signup(request):
+    return render(request, 'chats/signup.html')
