a02d77ba68d56b8f8230054f9393d0721ef0a190
saurabhraturi2000 <saurabh.raturi.999@gmail.com>
2023-03-14 19:47:52 +0530

    changes

--- chat_project/urls.py
+++ chat_project/urls.py
@@ -20,6 +20,7 @@ from chats import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
-    path('login/',views.login, name='login'),
-    path('signup/',views.signup, name='signup'),
+    path('login',views.login, name='login'),
+    path('signup',views.signup, name='signup'),
+    path('logoutuser',views.logoutuser, name='logoutuser')
 ]
--- chats/views.py
+++ chats/views.py
@@ -1,11 +1,44 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+
 
 # Create your views here.
 def home(request):
     return render(request, 'chats/index.html')
 
 def login(request):
-    return render(request, 'chats/login.html')
+    if request.method == 'POST':
+        name= request.POST['myname']
+        password = request.POST['mypassword']
+        user = authenticate(request, Username=name, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            return redirect('signup')
+
+    else: 
+        return render(request, 'chats/login.html')
 
 def signup(request):
-    return render(request, 'chats/signup.html')
+    if request.method == 'POST':
+        name = request.POST['myname']
+        email = request.POST['myemail']
+        password = request.POST['mypassword']
+        try:
+            user = User.objects.create_user(username=name,email=email,password=password)
+            user.save()
+            login(request, user)
+            redirect('/')
+        except IntegrityError:
+            return render(request, 'chats/signup.html',{'message':'Username already exists. Choose another one.'})
+        else:
+            redirect('/')
+    else: 
+        return render(request, 'chats/signup.html')
+
+def logoutuser(request):
+    logout(request)
+    return redirect('/')
