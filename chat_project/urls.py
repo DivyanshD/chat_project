62db952e1b2862e2b4f46b3762ed30053ea116f0
DivyanshD <divyanshsec23@gmail.com>
2023-03-15 20:14:06 +0530

    done

--- chat_project/urls.py
+++ chat_project/urls.py
@@ -17,10 +17,12 @@ from django.contrib import admin
 from django.urls import path
 from chats import views
 
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
-    # path('login',views.login, name='login'),
-    # path('signup',views.signup, name='signup'),
-    # path('logoutuser',views.logoutuser, name='logoutuser')
+    path('login',views.login, name='login'),
+    path('signup',views.signup, name='signup'),
+    path('logoutuser',views.logoutuser, name='logoutuser')
 ]
