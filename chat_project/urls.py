0ea7588881294675367e3c3fcd3e84a2be875279
DivyanshD <divyanshsec23@gmail.com>
2023-03-15 13:25:45 +0530

    semi coln

--- chat_project/urls.py
+++ chat_project/urls.py
@@ -22,5 +22,5 @@ urlpatterns = [
     path('',views.home, name='home'),
     path('login',views.login, name='login'),
     path('signup',views.signup, name='signup'),
-    path('logoutuser',views.logoutuser, name='logoutuser')
+    path('logoutuser',views.logoutuser, name='logoutuser'),
 ]
